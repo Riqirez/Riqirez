58e1e7d0d2c06586c77231efe73a82c8b39905a4
KimDaeHwan <daehwan112@gmail.com>
2025-03-16 13:59:46 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250316/비교 연산/comparison-operator.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	unsigned int a, b;
+	cin >> a >> b;
+	if (a >= b) {
+		cout << '1' << endl;
+	}
+	else {
+		cout << '0' << endl;
+	}
+
+	if (a > b) {
+		cout << '1' << endl;
+	}
+	else {
+		cout << '0' << endl;
+	}
+
+	if (b >= a) {
+		cout << '1' << endl;
+	}
+	else {
+		cout << '0' << endl;
+	}
+
+	if (b > a) {
+		cout << '1' << endl;
+	}
+	else {
+		cout << '0' << endl;
+	}
+
+	if (a == b) {
+		cout << '1' << endl;
+	}
+	else {
+		cout << '0' << endl;
+	}
+
+	if (a != b) {
+		cout << '1' << endl;
+	}
+	else {
+		cout << '0' << endl;
+	}
+
+	return 0;
+}
