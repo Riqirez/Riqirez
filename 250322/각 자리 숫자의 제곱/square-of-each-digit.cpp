ab953619bdca5620f73ce88d42b5b225c2577bf6
KimDaeHwan <daehwan112@gmail.com>
2025-03-22 18:14:30 +0900

    Title: 각 자리 숫자의 제곱, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250322/각 자리 숫자의 제곱/square-of-each-digit.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void num(int a,int &count) {
+	if (a <= 1)	return;
+
+	count += (a % 10) * (a % 10);
+	num(a / 10, count);
+}
+
+int main() {
+	int a,count=0;
+	cin >> a;
+
+	num(a, count);
+	cout << count << endl;
+	return 0;
+}
