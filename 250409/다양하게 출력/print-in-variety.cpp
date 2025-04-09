3c435a5f6324b196cffc6ef1795156de0a0c522d
KimDaeHwan <daehwan112@gmail.com>
2025-04-09 15:12:04 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250409/다양하게 출력/print-in-variety.cpp
+++ 250409/다양하게 출력/print-in-variety.cpp
@@ -5,7 +5,7 @@ int main() {
     cout << "Total days in Year" << endl;
     cout << "365" << endl;
     cout << "Circumference rate" << endl;
-    cout << 3.1415926535 << endl;
+    cout << "3.1415926535" << endl;
 
     return 0;
 }
