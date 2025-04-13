37256cd969ac8e15674651bfcd16ea4db88d530e
KimDaeHwan <daehwan112@gmail.com>
2025-04-13 16:36:03 +0900

    Title: 1이 되는 순간까지, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250413/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int f(int n) {
+    if (n <= 1) return 1;
+
+    if (n % 2) {
+        return f(n / 2)+ 1;
+    }
+    else {
+        return f(n / 3) +1;
+    }
+}
+
+int main() {
+    cin >> N;
+    cout << f(N) << endl;
+    return 0;
+}
