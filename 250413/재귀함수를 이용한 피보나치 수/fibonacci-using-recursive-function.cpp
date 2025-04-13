1f2d9bc2134d3a1430deadc4ff2277c3265832e1
KimDaeHwan <daehwan112@gmail.com>
2025-04-13 16:48:57 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250413/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int f(int n) {
+    if (n == 1) return 1;
+    if (n == 2) return 1;
+    return f(n - 1) + f(n - 2);
+}
+
+int main() {
+    cin >> N;
+    cout << f(N) << endl;
+    return 0;
+}
