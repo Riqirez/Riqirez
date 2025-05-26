228c5cd8c3a87e9a0243f3f3da740c66e559636b
KimDaeHwan <daehwan112@gmail.com>
2025-05-26 13:42:28 +0900

    Title: 1시간 뒤 시간 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250526/1시간 뒤 시간 출력/print-one-hour-later.cpp
+++ 250526/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -1,15 +1,9 @@
-
 #include <iostream>
 using namespace std;
 
 int main() {
-
-	int a, b;
-	char c;
-
-	cin >> a >> c >> b;
-	cout << a << endl << b;
-
-	return 0;
-
+    string a;
+    cin >> a;
+    cout << a << endl;
+    return 0;
 }
