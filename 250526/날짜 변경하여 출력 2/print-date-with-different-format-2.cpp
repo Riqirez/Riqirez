1286a4456823de6087fd5a33c718e5b2f90e2b7f
KimDaeHwan <daehwan112@gmail.com>
2025-05-26 13:47:25 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250526/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y, m, d;
+    cin >> m;
+    cin.get();
+    cin >> d;
+    cin.get();
+    cin >> y;
+    cin.get();
+
+    cout << y << '.' << m << '.' << d << endl;
+    return 0;
+}
