7c7a12357e3e0a06a79045e75122e5b9c857b1c3
KimDaeHwan <daehwan112@gmail.com>
2025-06-15 14:14:35 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250615/괄호 문자열의 적합성 판단/parentheses-string.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+string str;
+stack <char> s;
+
+int main() {
+    cin >> str;
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == '(') {
+            s.push('(');
+        }
+        else if (str[i] == ')') 
+        {
+
+            if (s.empty()) {
+                cout << "No" << endl;
+                return 0;
+            }
+            s.pop();
+        }
+    }
+    
+    if (!s.empty()) {
+        cout << "No" << '\n';
+    }
+    else cout << "Yes" << '\n';
+
+    return 0;
+}
