baa9647c15a8af99a1bf9c0e70155f0d011e9541
KimDaeHwan <daehwan112@gmail.com>
2025-06-18 21:13:49 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250618/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string arr;
+    getline(cin, arr);
+    cout << arr[0] << arr[1] << arr[2] << arr[3] << arr[9] << arr[10] << arr[11] << arr[12] <<
+        arr[8] << arr[4] << arr[5] << arr[6] << arr[7] << endl;
+ 
+    return 0;
+}
