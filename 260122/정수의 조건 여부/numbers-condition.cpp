63bacdc31553d4b9fc8ff44bc0b33334f99e25a9
KimDaeHwan <daehwan112@gmail.com>
2026-01-22 12:58:18 +0900

    Title: 정수의 조건 여부, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260122/정수의 조건 여부/numbers-condition.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int a;
+    cin >> a;
+    cout << (a>=113 ? 1 : 0) << '\n';
+
+    return 0;
+}
