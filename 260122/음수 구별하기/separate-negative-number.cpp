7ce8cf366f1e5fc9db38d2a79ce2c7955b8057fb
KimDaeHwan <daehwan112@gmail.com>
2026-01-22 12:45:09 +0900

    Title: 음수 구별하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260122/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int a;
+    cin >> a;
+    cout << a << '\n';
+    if(a<0) cout << "minus" << '\n';
+
+    return 0;
+}
