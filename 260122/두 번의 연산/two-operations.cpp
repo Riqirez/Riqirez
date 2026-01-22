fbf41425f43c62a8618cfbeb22da048488447485
KimDaeHwan <daehwan112@gmail.com>
2026-01-22 22:20:27 +0900

    Title: 두 번의 연산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260122/두 번의 연산/two-operations.cpp
+++ 260122/두 번의 연산/two-operations.cpp
@@ -8,9 +8,10 @@ int main(){
 
     int a;
     cin >> a;
-    if(a==5) cout << 'A';
-    if(a%2==0) cout << 'B';
-
+    if(a%2==1) a+=3;
+    if(a%3==0) a/=3;
+    
+    cout << a;
 
     return 0;
 }
