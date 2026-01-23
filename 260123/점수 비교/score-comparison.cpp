74fefe17412a0165be15fd55867eea7388ad8c4e
KimDaeHwan <daehwan112@gmail.com>
2026-01-23 14:49:19 +0900

    Title: 점수 비교, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260123/점수 비교/score-comparison.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a_math, a_eng, b_math, b_eng;
+    cin >> a_math >> a_eng >> b_math >> b_eng;
+
+    cout << (a_math > b_math) && (a_eng > b_eng);
+
+    return 0;
+}
