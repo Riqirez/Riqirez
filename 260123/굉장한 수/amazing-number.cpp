05a5d1bc532790d589efe5ff68741e4cdab4f9dd
KimDaeHwan <daehwan112@gmail.com>
2026-01-23 15:17:42 +0900

    Title: 굉장한 수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260123/굉장한 수/amazing-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a;
+    cin >> a;
+    cout << boolalpha;
+    cout << (a%2==1 && a%3==0) || (a%2==0 && a%5==0);
+    return 0;
+}
