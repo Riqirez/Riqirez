d6be7f1c2b034a2efad4115e964775e4ae0c4f6d
KimDaeHwan <daehwan112@gmail.com>
2026-01-23 15:08:06 +0900

    Title: 세 정수의 최솟값, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260123/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a,b,c;
+    cin >> a >> b >> c;
+    cout << min({a,b,c});
+
+    return 0;
+}
