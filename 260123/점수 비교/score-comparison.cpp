a49a59794335d29b98531f2fb75bb3d0f59faeff
KimDaeHwan <daehwan112@gmail.com>
2026-01-23 14:50:29 +0900

    Title: 점수 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260123/점수 비교/score-comparison.cpp
+++ 260123/점수 비교/score-comparison.cpp
@@ -9,7 +9,7 @@ int main() {
     int a_math, a_eng, b_math, b_eng;
     cin >> a_math >> a_eng >> b_math >> b_eng;
 
-    cout << (a_math > b_math) && (a_eng > b_eng);
+    cout << ((a_math > b_math) && (a_eng > b_eng));
 
     return 0;
 }
