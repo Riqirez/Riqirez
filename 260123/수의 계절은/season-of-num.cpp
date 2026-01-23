9737da8e4b8218031274bfed47bd6a30be8757cc
KimDaeHwan <daehwan112@gmail.com>
2026-01-23 15:22:25 +0900

    Title: 수의 계절은, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260123/수의 계절은/season-of-num.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a;
+    cin >> a;
+
+    if(a<=2) cout << "Winter";
+    else if(a<=5) cout << "Spring";
+    else if(a<=8) cout << "Summer";
+    else if(a<=11) cout << "Fall";
+    else cout << "Winter";
+    
+    return 0;
+}
