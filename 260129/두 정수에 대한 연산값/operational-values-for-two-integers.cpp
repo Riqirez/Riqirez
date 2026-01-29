2a11c2338eb58cd961bd47f952e68b7494b8c81a
KimDaeHwan <daehwan112@gmail.com>
2026-01-29 22:33:59 +0900

    Title: 두 정수에 대한 연산값, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260129/두 정수에 대한 연산값/operational-values-for-two-integers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int &small, int &big){
+    if(small > big){
+        int temp=small;
+        small = big;
+        big = small;
+    }
+    small*=2;
+    big+=25;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int s,b;
+    cin >> s >> b;
+    swap(s,b);
+    cout << s << ' ' << b;
+
+    return 0;
+}
