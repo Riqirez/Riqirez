6e3ee796ff7608897ecc7ec95e7db8eb21bcb026
KimDaeHwan <daehwan112@gmail.com>
2026-02-05 22:46:13 +0900

    Title: Date to Date, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260205/Date to Date/date-to-date.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int const arr[13] = {0,31,28,31,30,31,30,31,30,30,31,30,31};
+    int m1,d1,m2,d2,sum=1;
+    cin >> m1 >> d1 >> m2 >> d2;
+    
+    while(1){
+        if(m1==m2&&d1==d2) break;
+
+        ++sum;
+        ++d1;
+        if(d1 > arr[m1]){
+            ++m1;
+            d1=1;
+        }
+    }
+
+    cout << sum;
+    return 0;
+}
