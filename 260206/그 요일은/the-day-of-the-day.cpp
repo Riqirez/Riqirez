69e3d7ab6f385c5a72257a27408c5ff8c2f9f406
KimDaeHwan <daehwan112@gmail.com>
2026-02-06 13:36:13 +0900

    Title: 그 요일은, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260206/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+const int arr[14] = {0,0,31,60,91,121,152,182,213,244,274,305,335,366};
+const string day[7] = {"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+
+int myfind(string s){
+    for(int i=0; i<7; i++) if(day[i] == s) return i;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int m1, d1, m2, d2;
+    string s;
+    cin >> m1 >> d1 >> m2 >> d2;
+    cin >> s;
+
+    const int diff = ((arr[m2] + d2) - (arr[m1] + d1 + myfind(s)));
+
+    cout << diff/7+1;
+    return 0;
+}
