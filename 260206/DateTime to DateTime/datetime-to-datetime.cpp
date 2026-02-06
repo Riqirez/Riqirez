e50a03417c4efd77c125fb6819808d4ab4c00bf0
KimDaeHwan <daehwan112@gmail.com>
2026-02-06 12:56:58 +0900

    Title: DateTime to DateTime, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260206/DateTime to DateTime/datetime-to-datetime.cpp
+++ 260206/DateTime to DateTime/datetime-to-datetime.cpp
@@ -6,31 +6,10 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int a,b,c,sum=0;
-    cin >> a >> b >> c;
+    int d,h,m;
+    cin >> d >> h >> m;
 
-    while(1){
-        if(a==b&&b==c&&c==11) break;
-        if(c!=11){
-            --c;
-            ++sum;
-        }
-        else if(c<0){
-            --b;
-            c=59;
-            ++sum;
-        }
-        else{
-            
-            if(b<0){
-                b=23;
-                --a;
-            }
-            --b;
-            sum+=60;
-        }
-    }
+    cout << (d*24*60 + h*60 + m) - (11*24*60 + 11*60 + 11);
 
-    cout << sum;
     return 0;
 }
