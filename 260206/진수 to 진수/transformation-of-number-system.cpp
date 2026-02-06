ec5ddb8a432d998312e815773d62bdb82c335aa5
KimDaeHwan <daehwan112@gmail.com>
2026-02-06 22:13:26 +0900

    Title: 진수 to 진수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260206/진수 to 진수/transformation-of-number-system.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    const string table = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int a, b, sum=0;
+    string n = "";
+    string s = "";
+    cin >> a >> b >> n;
+
+    while(n.size()){
+        sum = sum*a + (table[n[n.size()-1]%a]-'0');
+        n.pop_back();
+    }
+
+    while(sum){
+        s.push_back(sum%b + '0');
+        sum/=b;
+    }
+    reverse(s.begin(), s.end());
+    cout << s;
+    return 0;
+}
