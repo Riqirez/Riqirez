701e8c601f3b5e1f1f3d95225ba5ef161a9fd733
KimDaeHwan <daehwan112@gmail.com>
2026-02-07 13:38:56 +0900

    Title: 최대로 겹치는 지점, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260207/최대로 겹치는 지점/maximum-overlapped-points.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    const int T = 100;
+    int arr[201] = {0};
+    int N;
+    cin >> N;
+    while(N--){
+        int x, y;
+        cin >> x >> y;
+        for(int i=x+T; i<=y+T; i++) ++arr[i];
+    }
+
+    cout << *max_element(arr,arr+200);
+    return 0;
+}
